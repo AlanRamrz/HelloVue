e7d9a054af8c6082ee1f225af7a9f47b4be2a3c1
Alan Ramirez <alanramirez@MacBook-Pro-de-Alan.local>
2018-09-11 11:38:46 -0500

    Directive for rendering an element or not

--- js/index.js
+++ js/index.js
@@ -1,6 +1,13 @@
-var hello = new Vue({
-  el: '#hello',
+var ex1 = new Vue({
+  el: '#ex1',
   data: {
     message: "Hey there, we are MiniCoding..."
+  }
+})
+
+var ex2 = new Vue({
+  el: '#ex2',
+  data: {
+    visible: true
   }
 })
