b07909bb5cebc78765713fe2c7f02495887e9d49
Alan Ramirez <alanramirez@MacBook-Pro-de-Alan.local>
2018-09-11 13:11:30 -0500

    Last examples and ToDoList app

--- js/index.js
+++ js/index.js
@@ -23,3 +23,41 @@ var ex3 = new Vue({
     }
   }
 })
+
+var ex4 = new Vue({
+  el: '#ex4',
+  data: {
+    variable: "MiniCoding"
+  }
+})
+
+var ex5 = new Vue({
+  el: '#ex5',
+  data: {
+    count: 0
+  },
+  methods: {
+    add: function(){
+      this.count += 1;
+    }
+  }
+})
+
+var toDoApp = new Vue({
+  el: '#toDoExample',
+  data: {
+    toDos: ["Follow @MiniCoding","RT this"],
+    newToDo: ""
+  },
+  methods: {
+    add: function(){
+      if(this.newToDo){
+        this.toDos.push(this.newToDo);
+        this.newToDo = "";
+      }
+    },
+    remove: function(index){
+      this.toDos.splice(index, 1);
+    }
+  }
+})
